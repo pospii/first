53a2bda93cf18c521b53a1f39f45ee55ade2447b
Adam Pospíšek <adam.pospisek@email.cz>
2021-11-12 15:44:06 +0100

    Kalkulačka umí počítat, nikdy se ale nezastaví

--- kalkulacka.py
+++ kalkulacka.py
@@ -1,6 +1,28 @@
-print("Vítejte v kalkulačce!")
+print("Vítejte v kalkulačce!\n")
+
 
 while True:
-    print("Zadejte číslo, které určuje vámi vybranou operaci.\n1 - sčítání\n2 - odečítání\n3 - násobení\n4 - dělení")
-    operace = input()
+    print("Vyberte si operaci:\n1 - sčítání\n2 - odečítání\n3 - násobení\n4 - dělení")
+    operace = int(input())
+    if operace == 1 or operace == 2 or operace == 3 or operace == 4:
+        print("Nyní zadejte postupně dvě čísla:")
+        a = int(input("a = "))
+        b = int(input("b = "))
+        if operace == 1:
+            vysledek = a + b
+            print(f"Výsledek: {vysledek}")
+        elif operace == 2:
+            vysledek = a - b
+            print(f"Výsledek: {vysledek}")
+        elif operace == 3:
+            vysledek = a * b
+            print(f"Výsledek: {vysledek}")
+        elif operace == 4:
+            vysledek = a / b
+            print(f"Výsledek: {vysledek}")
+        else:
+            print("Nevím jak se ti to podařilo, ale rozbils to")
+    else:
+        print("Zadejte pouze číslo podporované operace!")
+    break
     
