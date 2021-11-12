ff514535e938c06c312e49f1fc748c0785dbd0ee
Adam Pospíšek <adam.pospisek@email.cz>
2021-11-12 16:51:07 +0100

    Celková úprava kódu, vymazání while, plně funkční

--- kalkulacka.py
+++ kalkulacka.py
@@ -1,7 +1,20 @@
-print("Vítejte v kalkulačce!\n")
+vysledky = []
 
+def pozdrav():
+    print("Vítejte v kalkulačce!\n")
 
-while True:
+def pokracovani():
+    pokracovat = input("Přejete si pokračovat v počítání? ano/ne\n")
+    if pokracovat == "ano":
+        kalkulacka()
+    elif pokracovat == "ne":
+        print("V tom případě děkuji za použití kalkulačky.")
+        print (f"Všechny vaše výsledky: {vysledky}")
+    else:
+        print("Zadejte pouze ano nebo ne")
+        pokracovani()
+
+def kalkulacka():
     print("Vyberte si operaci:\n1 - sčítání\n2 - odečítání\n3 - násobení\n4 - dělení")
     operace = int(input())
     if operace == 1 or operace == 2 or operace == 3 or operace == 4:
@@ -9,20 +22,20 @@ while True:
         a = int(input("a = "))
         b = int(input("b = "))
         if operace == 1:
-            vysledek = a + b
-            print(f"Výsledek: {vysledek}")
+            vysledek = a + b  
         elif operace == 2:
-            vysledek = a - b
-            print(f"Výsledek: {vysledek}")
+            vysledek = a - b   
         elif operace == 3:
             vysledek = a * b
-            print(f"Výsledek: {vysledek}")
         elif operace == 4:
             vysledek = a / b
-            print(f"Výsledek: {vysledek}")
         else:
             print("Nevím jak se ti to podařilo, ale rozbils to")
+        print(f"Výsledek: {vysledek}")
+        vysledky.append(vysledek)
     else:
         print("Zadejte pouze číslo podporované operace!")
-    break
-    
+    pokracovani()
+
+pozdrav()
+kalkulacka()
