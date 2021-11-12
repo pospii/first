b2c7ce5e989fcf523ddbca7255d722b333828f9a
Adam Pospíšek <adam.pospisek@email.cz>
2021-11-12 15:04:20 +0100

    First commit

---
+++ kalkulacka.py
@@ -0,0 +1,6 @@
+print("Vítejte v kalkulačce!")
+
+while True:
+    print("Zadejte číslo, které určuje vámi vybranou operaci.\n1 - sčítání\n2 - odečítání\n3 - násobení\n4 - dělení")
+    operace = input()
+    
